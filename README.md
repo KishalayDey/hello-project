00ca2f64211f10586d17088c2ee9dd7c5ff52438
KishalayDey <23-55682-3@student.aiub.edu>
2026-01-29 03:29:35 +0600

    Initial commit: Add hello.py and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Project
+
+This is a simple Python program that asks for your name and prints the current date and time.
---
+++ hello.py
@@ -0,0 +1,7 @@
+import datetime
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
+now = datetime.datetime.now()
+print("Current date and time:", now)
